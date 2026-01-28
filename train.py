4717e80cbc6e04b4449375f29725ce70508328fc
yvnphanikishore <52032839+yvnphanikishore@users.noreply.github.com>
2026-01-28 20:20:55 +0530

    Update train.py

--- train.py
+++ train.py
@@ -98,6 +98,22 @@ class ChurnModelPipeline:
         with open("metrics.txt", "w") as outfile:
             outfile.write(f"Accuracy = {round(accuracy, 2)}, F1 Score = {round(f1, 2)}\n")
         print("Metrics saved in 'metrics.txt'.")
+    def plot_roc_curve(self):
+        print("Plotting ROC Curve...")
+        y_probs = self.model_pipeline.predict_proba(self.X_test)[:, 1]
+        fpr, tpr, _ = roc_curve(self.y_test, y_probs)
+
+        plt.figure(figsize=(8, 6))
+        plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc_score(self.y_test, y_probs):.2f})')
+        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver Operating Characteristic (ROC) Curve')
+        plt.legend(loc="lower right")
+        plt.savefig("roc_curve.png", dpi=120)
+        print("ROC curve saved as 'roc_curve.png'.")
 
     def save_pipeline(self):
         print("Saving pipeline to file...")
@@ -125,6 +141,7 @@ if __name__ == "__main__":
     # plot CM and save the metrics
     churn_pipeline.plot_confusion_matrix()
     churn_pipeline.save_metrics(accuracy, f1)
+    churn_pipeline.plot_roc_curve()
 
     # Save the pipeline
     churn_pipeline.save_pipeline()
